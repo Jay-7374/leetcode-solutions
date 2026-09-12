7b51abbea0dd482c08041bbe00ee2fe6ed0e4a79
VEERAMOSU VENKATA JAYA PRAKASH <jayaprakash99956@gmail.com>
2026-09-12 20:01:32 +0530

    Time: 1615 ms (38.98%) | Memory: 86.9 MB (33.90%) - LeetSync

---
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.py
@@ -0,0 +1,63 @@
+class Solution:
+    def maximumWeight(self, intervals: List[List[int]]) -> List[int]:
+
+        import bisect
+
+        n = len(intervals)
+
+        arr = []
+
+        for i in range(n):
+            l, r, w = intervals[i]
+            arr.append((r, l, w, i))
+
+        # Sort according to right endpoint
+        arr.sort()
+
+        ends = [x[0] for x in arr]
+
+        # dp[i][k] = best answer using first i intervals
+        # with at most k intervals
+        dp = [[(0, []) for _ in range(5)] for _ in range(n + 1)]
+
+        for i in range(1, n + 1):
+
+            r, l, w, idx = arr[i - 1]
+
+            for k in range(1, 5):
+
+                # Don't take current interval
+                dp[i][k] = dp[i - 1][k]
+
+                # Find number of intervals whose end < current start
+                p = bisect.bisect_left(ends, l)
+
+                prev_weight, prev_indices = dp[p][k - 1]
+
+                take_weight = prev_weight + w
+                take_indices = sorted(prev_indices + [idx])
+
+                # Take current interval
+                if take_weight > dp[i][k][0]:
+                    dp[i][k] = (take_weight, take_indices)
+
+                elif take_weight == dp[i][k][0]:
+                    if take_indices < dp[i][k][1]:
+                        dp[i][k] = (take_weight, take_indices)
+
+        # Find maximum score among 0,1,2,3,4 intervals
+        ans_weight = 0
+        ans = []
+
+        for k in range(1, 5):
+
+            weight, indices = dp[n][k]
+
+            if weight > ans_weight:
+                ans_weight = weight
+                ans = indices
+
+            elif weight == ans_weight and indices < ans:
+                ans = indices
+
+        return ans
